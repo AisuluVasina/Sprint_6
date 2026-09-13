dacde003747077f96aeb27e4a7f5242adac14dda
Vasina Aisylu <aisulu_pochta@mail.ru>
2026-09-13 22:56:14 +0300

    fix: исправлен файл README

--- urls.py
+++ urls.py
@@ -1,4 +1,4 @@
 class Urls:
     MAIN_PAGE = "https://qa-scooter.praktikum-services.ru/"
-    ORDER_PAGE = "https://qa-scooter.praktikum-services.ru/order" # Или какой там у вас URL страницы заказа
+    ORDER_PAGE = "https://qa-scooter.praktikum-services.ru/order" 
     DZEN_URL = "dzen.ru"
